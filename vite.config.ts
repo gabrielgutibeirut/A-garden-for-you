57e32186d6aef9bc893942c44da851c9ca34f9de
gabrielgutibeirut <gabrielgutibeirut@gmail.com>
2026-09-22 00:55:33 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/A-garden-for-you/',
   plugins: [react()],
 })
